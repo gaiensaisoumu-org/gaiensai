2b6a1519ec092bd1b8af4669978bd07738f6827c
Rio Gunawan <ribbongrapes@gmail.com>
2026-08-02 20:48:55 +0900

    校内マップを今年版に差し替え

--- src/pages/user/Map.tsx
+++ src/pages/user/Map.tsx
@@ -1,6 +1,8 @@
 import { useTitle } from '../../hooks/useTitle';
 import styles from '../../styles/sub-pages.module.css';
-import mapImage from '../../assets/map.jpg';
+import mapImage from '../../assets/map.webp';
+import mapImage1 from '../../assets/map1.webp';
+import mapImage2 from '../../assets/map2.webp';
 import Modal2 from '../../components/ui/Modal2';
 
 const Map = () => {
@@ -10,7 +12,15 @@ const Map = () => {
       <Modal2 />
       <h1 className={styles.pageTitle}>校内マップ</h1>
 
-      <img src={mapImage} alt='校内マップ' className={styles.map} />
+      <img
+        src={mapImage}
+        alt='校内マップ'
+        className={`${styles.map} ${styles.desktopMap}`}
+      />
+      <div className={styles.mobileMapImages}>
+        <img src={mapImage1} alt='校内マップ（前半）' className={styles.map} />
+        <img src={mapImage2} alt='校内マップ（後半）' className={styles.map} />
+      </div>
 
       {/* 別のタブで開くボタンを追加 */}
       <div className={styles.buttonContainer}>
