9e0465ae2b34485833310db6e4ae5d074005f0a4
Rio Gunawan <ribbongrapes@gmail.com>
2026-08-03 00:52:25 +0900

    AdminHomeのURLを削除

--- src/pages/admin/AdminHome.tsx
+++ src/pages/admin/AdminHome.tsx
@@ -1,5 +1,4 @@
 import styles from '../../styles/sub-pages.module.css';
-import { preload, Scan } from '../../routes';
 import { useTitle } from '../../hooks/useTitle';
 
 const AdminHome = () => {
@@ -12,34 +11,6 @@ const AdminHome = () => {
         <p>
           セキュリティ上の理由から、各ページへのURLは貼っていません。Teamsのマニュアルからそれぞれのページへアクセスしてください。
         </p>
-      </section>
-      <section>
-        <h2>リンク集</h2>
-        <p>
-          開発中はリンクがないと不便なので、仮としてリンクを貼っておきます。本番なのにリンクが残っていることに気づいた方は至急お知らせください。
-        </p>
-        <ul>
-          <li>
-            <a href='/admin/scan' onMouseEnter={() => preload(Scan)}>
-              カメラ使用のスキャンページ
-            </a>
-          </li>
-          <li>
-            <a href='/admin/register' onMouseEnter={() => preload(Scan)}>
-              スキャナ用チケットスキャンページ
-            </a>
-          </li>
-          <li>
-            <a href='/admin/history' onMouseEnter={() => preload(Scan)}>
-              チケットスキャン履歴
-            </a>
-          </li>
-          <li>
-            <a href='/admin/settings' onMouseEnter={() => preload(Scan)}>
-              コントロールパネル
-            </a>
-          </li>
-        </ul>
       </section>
     </div>
   );
