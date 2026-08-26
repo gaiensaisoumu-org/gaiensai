2762902316318cfb53fc05d403810032d5cb3ddd
Rio Gunawan <ribbongrapes@gmail.com>
2026-08-27 02:05:50 +0900

    /admin/availabilityの認証を外す

--- src/pages/admin/Availability.tsx
+++ src/pages/admin/Availability.tsx
@@ -1,15 +1,12 @@
 import PerformancesTable from '../../features/performances/PerformancesTable';
-import { AdminAuthLayout } from '../../layout/AdminAuthLayout';
 
 const Availability = () => (
-  <AdminAuthLayout title='公演空き状況' minimal>
-    <PerformancesTable
-      orientation='classes-as-columns'
-      remainingMode='total'
-      showFilters={false}
-      availabilitySource='monitor'
-    />
-  </AdminAuthLayout>
+  <PerformancesTable
+    orientation='classes-as-columns'
+    remainingMode='total'
+    showFilters={false}
+    availabilitySource='monitor'
+  />
 );
 
 export default Availability;
